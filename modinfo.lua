61538a900d32644d67dd0b907f7347a1c81fe793
folknor <folk@folk.wtf>
2017-10-01 02:52:30 +0200

    Initial upload. Research+mining+energy components are at least usable to a high enough degree.

---
+++ modinfo.lua
@@ -0,0 +1,11 @@
+-- modinfo read by stlrel from http://github.com/stellaris-mods/scripts
+return {
+	path = "autoass",
+	name = "Autonomous Assembler",
+	tags = { "Gameplay", "Spaceships" },
+	picture = "thumb.png",
+	supported_version = "1.8.*",
+	remote_file_id = false,
+	--readme = "readme.md",
+	--steambb = "steam.bbcode",
+}
