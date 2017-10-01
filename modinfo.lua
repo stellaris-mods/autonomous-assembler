cb4e5408d0fd894d577f81a8535f323296e47334
folknor <folk@folk.wtf>
2017-10-01 02:55:29 +0200

    Update modinfo.

--- modinfo.lua
+++ modinfo.lua
@@ -3,9 +3,14 @@ return {
 	path = "autoass",
 	name = "Autonomous Assembler",
 	tags = { "Gameplay", "Spaceships" },
-	picture = "thumb.png",
+	picture = false,
 	supported_version = "1.8.*",
 	remote_file_id = false,
 	--readme = "readme.md",
 	--steambb = "steam.bbcode",
+	-- Files and folders added in exclude will not be copied over to the Paradox Interactive
+	-- local mod folder when you run `stlrel --steam`.
+	exclude = {
+		"source"
+	}
 }
